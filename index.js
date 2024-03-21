1242b9239d916401ebf3fcaa3c5b0ac65282845c
Nícolas Ferreira <nicolasemanuel.f@gmail.com>
2024-03-21 15:03:48 -0300

    Atualizando algoritmo de saída

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("Iniciando o desafio sobre as classes de um jogo")
+class hero {
+    constructor (name, age, classType) {
+        this.name = name
+        this.age = age
+        this.classType = classType
+    }
+
+    attack() {
+        const attackType = {
+            mago: "magia",
+            guerreiro: "espada",
+            monge: "artes marciais",
+            ninja: "shuriken"
+        }
+
+        switch (this.classType) {
+            case "mago":
+                console.log(`O ${this.classType} atacou usando ${attackType.mago}.`)
+                break
+            case "guerreiro":
+                console.log(`O ${this.classType} atacou usando ${attackType.guerreiro}.`)
+                break
+            case "monge":
+                console.log(`O ${this.classType} atacou usando ${attackType.monge}.`)
+                break
+            case "ninja":
+                console.log(`O ${this.classType} atacou usando ${attackType.ninja}.`)
+                break
+            default:
+                console.log(`O herói ${this.name} não possui uma classe conhecida.`)
+        }
+    }
+}
+
+let novoHeroi = new hero("Owen Higgins", 20, "guerreiro")
+
+novoHeroi.attack()
